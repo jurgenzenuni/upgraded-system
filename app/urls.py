71d8f4e21e4364927ee3ee9441103938f65c1eae
jurg <jurgen.zenuni@outlook.com>
2024-09-08 15:57:38 -0400

    updated settings static root

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from wallify.views import homepage
 from wallify.views import about
 from wallify import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +24,3 @@ urlpatterns = [
      path('delete_favorite_image/', views.delete_favorite_image, name='delete_favorite_image'),
     path('generate-image/', views.generate_image, name='generate_image'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
